94aa0f671f131aefa35846c5501c06f2b25e9448
Azim Shah <azim.1998@hotmail.com>
2022-10-16 12:01:52 +0100

    initialise project

---
+++ app/src/main/java/com/example/bankingapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bankingapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bankingapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
